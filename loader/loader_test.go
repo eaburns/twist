b1c278b723b5a06879939376fb79e9e24d71f900
Ethan Burns <burns.ethan@gmail.com>
2014-11-09 11:37:21 -0500

    Add a test for Program.Node and Program.Comments.

--- loader/loader_test.go
+++ loader/loader_test.go
@@ -1,6 +1,13 @@
 package loader
 
-import "testing"
+import (
+	"go/ast"
+	"reflect"
+	"strings"
+	"testing"
+
+	"code.google.com/p/go.tools/go/ssa"
+)
 
 // TestNewProgramString just tests some simple cases
 // for the presence of an error or not.
@@ -55,4 +62,96 @@ func TestProgramFunction(t *testing.T) {
 		}
 	}
 
+}
+
+// TestProgramNode tests that
+// the Program.Node map associates
+// ssa.Instructions with the desired ast.Nodes.
+func TestProgramNode(t *testing.T) {
+	// For each comment, the test ensures that
+	// there is an instruction of the appropriate type
+	// mapped to a node of the appropriate type
+	// which is associated with the comment.
+	// Annotations are type names of the form:
+	// 	ssa.Instruction:ast.Node
+	src := `
+		package main
+		var ch = make(chan bool)
+		func main() {
+			// If:ForStmt
+			for i := 0; i < 5; i++ /* Jump:IncDecStmt */ {
+				// If:IfStmt
+				if /* UnOp:ParenExpr*/ (<- ch /* UnOp:Ident */)  {
+					panic("Whoa!") // Panic:ExprStmt
+				} else {
+					/* Send:SendStmt */ ch /* UnOp:Ident */ <- true
+				}
+			}
+		}`
+	prog, err := NewProgramString(src)
+	if err != nil {
+		panic(err)
+	}
+
+	// Gather type annotations from comments.
+	instrType := make(map[*ast.CommentGroup]string)
+	nodeType := make(map[*ast.CommentGroup]string)
+	for _, c := range prog.Comments {
+		if len(c) != 1 {
+			panic("annotations must only contain one comment")
+		}
+		fields := strings.Split(c[0].Text(), ":")
+		instrType[c[0]] = strings.TrimSpace(fields[0])
+		nodeType[c[0]] = strings.TrimSpace(fields[1])
+	}
+
+	f, err := prog.Function("main", "main")
+	if err != nil {
+		panic(err)
+	}
+	for _, blk := range f.Blocks {
+		for _, p := range blk.Instrs {
+			if _, ok := p.(*ssa.DebugRef); ok {
+				continue
+			}
+			n, ok := prog.Node[p]
+			if !ok { // no node
+				continue
+			}
+			c, ok := prog.Comments[n]
+			if !ok { // no comment
+				continue
+			}
+			if len(c) != 1 {
+				panic("annotations must only contain one comment")
+			}
+
+			// Found an instruction with a node and a comment.
+			// Get the desired types.
+			wantIt, ok := instrType[c[0]]
+			if !ok {
+				t.Errorf("unexpected comment at: %s", prog.Position(n.Pos()))
+				continue
+			}
+			wantNt, ok := nodeType[c[0]]
+			if !ok {
+				panic("instrType and nodeType out of sync")
+			}
+			delete(instrType, c[0])
+			delete(nodeType, c[0])
+
+			// Check against the types we got.
+			it := reflect.ValueOf(p).Elem().Type().Name()
+			nt := reflect.ValueOf(n).Elem().Type().Name()
+			if it != wantIt {
+				t.Errorf("got instruction type %s, want %s", it, wantIt)
+			}
+			if nt != wantNt {
+				t.Errorf("got node type %s, want %s", nt, wantNt)
+			}
+		}
+	}
+	for c := range instrType {
+		t.Errorf("no instruction for %s", c.Text())
+	}
 }
